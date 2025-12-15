654a12f797e8bc22c880c0136611c5cbac453e91
Raave Aires <developer@raave.me>
2025-12-14 21:59:39 -0300

    feat: implementa as novas tabelas de memros e usuários adicionáveis.

--- app/(main)/org/[slug]/page.tsx
+++ app/(main)/org/[slug]/page.tsx
@@ -1,22 +1,24 @@
+// libs e funções:
 import { getOrganizationBySlug } from "@/auth/actions/organizations";
-import { MembersTable } from "@/components/organization/members-table";
 import { getAllUsers } from "@/auth/actions/users";
-import { AllUsers } from "@/components/organization/all-users";
+
+// componentes
+import { AllMembers } from "@/components/organization/tables/members/all-members";
+import { AllUsers } from "@/components/organization/tables/users/all-users";
 
 type Params = Promise<{ slug: string }>;
 
 export default async function OrganizationPage({ params }: { params: Params }) {
   const { slug } = await params;
-  const [ organization, users ] = await Promise.all([
+  const [organization, users] = await Promise.all([
     getOrganizationBySlug(slug),
     getAllUsers(),
   ]);
 
-  return(
-    <>
-      <h2 className="text-2xl font-bold font-averia">{organization?.name}</h2>
-      <MembersTable members={organization?.members || []} />
+  return (
+    <div className="flex flex-col gap-4">
+      <AllMembers members={organization?.members || []} />
       <AllUsers users={users || []} />
-    </>
+    </div>
   );
 }
