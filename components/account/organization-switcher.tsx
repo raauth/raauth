b1c6dc02964d0cb71cce458944ca82554cf50f0c
Raave Aires <developer@raave.me>
2025-12-15 09:09:45 -0300

    fix: substitui o componente local pelo que estava no github

--- components/account/organization-switcher.tsx
+++ components/account/organization-switcher.tsx
@@ -1,64 +1,3 @@
-<<<<<<< HEAD
-"use client";
-
-import { toast } from "sonner";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import type { Organization } from "@/prisma/client/client";
-import { authClient } from "@/lib/auth-client";
-
-type OrganizationSwitcherProps = {
-  organizations: Organization[];
-};
-
-export function OrganizationSwitcher({
-  organizations,
-}: OrganizationSwitcherProps) {
-  const { data: activeOrganization } = authClient.useActiveOrganization();
-
-  const handleChangeOrganization = async (organizationId: string) => {
-    try {
-      const { error } = await authClient.organization.setActive({
-        organizationId,
-      });
-
-      if (error) {
-        console.error(error);
-        toast.error("Failed to switch organization");
-        return;
-      }
-
-      toast.success("Organization switched successfully");
-    } catch (error) {
-      console.error(error);
-      toast.error("Failed to switch organization");
-    }
-  };
-
-  return (
-    <Select
-      onValueChange={handleChangeOrganization}
-      value={activeOrganization?.id}
-    >
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Theme" />
-      </SelectTrigger>
-      <SelectContent>
-        {organizations.map((organization) => (
-          <SelectItem key={organization.id} value={organization.id}>
-            {organization.name}
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
-  );
-}
-=======
 "use client"
 
 // libs e funções:
@@ -177,4 +116,3 @@ export function OrganizationSwitcher({ organizations }: OrganizationSwitcherProp
     </DropdownMenuSub>
   )
 }
->>>>>>> 1ecf572a5d6f511976ae300ee7457ebd1908b02a
