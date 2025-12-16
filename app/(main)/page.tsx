1ae53c95ff9e7d36a5337434ce0f6af7b2ece3d9
Raave Aires <developer@raave.me>
2025-12-16 09:38:40 -0300

    refactor: implementa o card de dashboard como componente reutilizável

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,31 +1,9 @@
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Copy } from "lucide-react";
-import Link from "next/link";
+import { DashboardsCard } from "@/app/(main)/dashboards-cards/dashboard-card";
 
-export default  function Page() {
+export default function Page() {
   return (
     <>
-     <Card className="w-sm">
-      <CardHeader>
-        <CardTitle className="text-2xl">
-          Inventário de TI
-        </CardTitle>
-        <CardDescription>
-          Vê o dashboard do inventário de TI
-        </CardDescription>
-      </CardHeader>
-      <CardFooter className="gap-2">
-        <Button asChild>
-          <Link href="/inventory">
-            Abrir
-          </Link>
-        </Button>
-        <Button>
-          <Copy/> Copiar link
-        </Button>
-      </CardFooter>
-     </Card>
+      <DashboardsCard title="Inventário de TI" description="Vê o dashboard do inventário de TI" href="/inventory" />
     </>
   );
 }
